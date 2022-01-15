02eb9b23c11c952a42ff2b96d99194b69f7ff825
unknown <mperkovic@protonmail.com>
2022-01-15 23:11:38 +0100

    Add yargs

--- app.js
+++ app.js
@@ -1,7 +1,49 @@
 const notes = require('./notes');
+const yargs = require('yargs');
 const chalk = require('chalk');
 
-const msg = notes.getNotes();
-
-console.log(msg);
-console.log(chalk.blue('I\'m blue dabudidabuda'));
+// Customize yargs version
+yargs
+    .version('1.1.0')
+    .command({
+        command: 'add',
+        describe: 'Add a new note',
+        builder: {
+            title: {
+                describe: 'Note title',
+                demandOption: true,
+                type: 'string'
+            },
+            body: {
+                describe: 'Note body',
+                demandOption: true,
+                type: 'string'
+            }
+        },
+        handler: (argv) => {
+            console.log(`Title: ${argv.title}`);
+            console.log(`Body: ${argv.body}`);
+        }
+    })
+    .command({
+        command: 'remove',
+        describe: 'Remove a note',
+        handler: () => {
+            console.log('Removing the note!')
+        }
+    })
+    .command({
+        command: 'list',
+        describe: 'List the notes',
+        handler: () => {
+            console.log('Listing the notes!')
+        }
+    })
+    .command({
+        command: 'read',
+        describe: 'Read a note',
+        handler: () => {
+            console.log('Reading the note!')
+        }
+    })
+    .argv;
